e3e11765c21edeef93739614ae398270175ae53f
tracer-ysyx <tracer@ysyx.org>
2023-10-15 16:15:24 +0800

    > sim RTL ysyx_22040000 龚开宸 Linux trace-hlylwxx9 6.1.55-1-MANJARO #1 SMP PREEMPT_DYNAMIC Sat Sep 23 12:13:56 UTC 2023 x86_64 GNU/Linux  16:15:24 up 19:38,  1 user,  load average: 1.16, 0.80, 0.75

---
+++ npc/lab2/vsrcs/top.v
@@ -0,0 +1,69 @@
+module top(
+	input clk,
+	input rst,
+	input [8:0] sw,
+	output [2:0] led,
+	output reg [7:0] seg1
+);
+
+output reg [2:0] out;
+
+enc38 enc381(
+	.in(sw[7:0]),
+	.en(sw[8]),
+	.out(out)
+);
+
+bcd7seg bcd7seg1(
+	.in(out),
+	.seg(seg1)
+);
+
+endmodule
+
+module enc38(
+	input [7:0] in,
+	input en,
+	output reg [2:0] out
+);
+
+always @(*) begin
+	if (en) begin
+	casez (in)
+		8'bzzzzzzz1: out = 3'b000;
+		8'bzzzzzz10: out = 3'b001;
+		8'bzzzzz100: out = 3'b010;
+		8'bzzzz1000: out = 3'b011;
+		8'bzzz10000: out = 3'b100;
+		8'b11100000: out = 3'b101;
+		8'b11000000: out = 3'b110;
+		8'b10000000: out = 3'b111;
+		default: out = 3'b000;
+	endcase
+	end
+	else begin
+		out = 3'b000
+	end
+end
+
+endmodule
+
+module bcd7seg(
+	input reg [2:0] in,
+	output reg [7:0] seg
+)
+
+always @(*) begin
+	casez (in)
+		3'b000: segs = 8'b11111101;
+		3'b001: segs = 8'b01100000;
+		3'b010: segs = 8'b11011010;
+		3'b011: segs = 8'b11110010;
+		3'b100: segs = 8'b01100110;
+		3'b101: segs = 8'b10110110;
+		3'b110: segs = 8'b10111110;
+		3'b111: segs = 8'b11100000;
+		default: out = 8'b11111111;
+	endcase
+end
+endmodule

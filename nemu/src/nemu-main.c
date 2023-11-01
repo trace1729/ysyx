a4d7827f2261437a5d28aebd6c5f2425df159f09
tracer-ysyx <tracer@ysyx.org>
2023-11-01 11:42:37 +0800

    > compile NEMU ysyx_22040000 龚开宸 Linux trace-hlylwxx9 6.1.55-1-MANJARO #1 SMP PREEMPT_DYNAMIC Sat Sep 23 12:13:56 UTC 2023 x86_64 GNU/Linux  11:42:37 up 15:32,  1 user,  load average: 0.46, 0.60, 0.51

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
   assert(fp != NULL);
   char buf[65536+10];
   bool success;
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < 9000; i++) {
     // read oneline into the buf; 
     // will continue read the last line of the file
     fgets(buf, ARRLEN(buf), fp);
@@ -54,6 +54,7 @@ int main(int argc, char *argv[]) {
     *(c_expr + strlen(c_expr) - 1) = '\0';
     unsigned int actual = expr(c_expr, &success);
     Check(res == actual, "failed on #%d, expression is %s, expected %u , but %u", i, c_expr, res, actual) ;
+    printf("PASSED #%d\n", i);
   }
 
 error:

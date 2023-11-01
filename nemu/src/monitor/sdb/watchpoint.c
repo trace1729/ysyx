6a54595c446ea858d84bce2cac8879a8c52accf3
tracer-ysyx <tracer@ysyx.org>
2023-11-01 21:38:34 +0800

    > compile NEMU ysyx_22040000 龚开宸 Linux trace-hlylwxx9 6.1.55-1-MANJARO #1 SMP PREEMPT_DYNAMIC Sat Sep 23 12:13:56 UTC 2023 x86_64 GNU/Linux  21:38:34 up 1 day,  1:28,  1 user,  load average: 0.49, 0.41, 0.43

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -13,21 +13,24 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "debug.h"
 #include "sdb.h"
+#include "watchpoint.h"
+#include <assert.h>
+#include <stdio.h>
 
 #define NR_WP 32
-
-typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
-
-  /* TODO: Add more members if necessary */
-
-} WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
+
+// setup #id for each watch point node, 
+// and link the (next) field of wp node to the following node
+// the next field of the last wp node points to NULL
+
+// head points to the first wp node in use
+// free_ points to the free wp nodes
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
@@ -39,5 +42,79 @@ void init_wp_pool() {
   free_ = wp_pool;
 }
 
-/* TODO: Implement the functionality of watchpoint */
+WP* new_wp() {
+  assert(free_ != NULL);
+  WP* backup = head;
+  head = free_;
+  free_ = free_->next;
+  head->next = backup;
+  return head;
+}
 
+void free_wp(int NO) {
+  assert(head != NULL);
+  WP* wp, *backup;
+  // free head
+  if (head->NO == NO) {
+    backup = free_;
+    free_ = head;
+    head = head->next;
+    free_->next = backup;
+    return;
+  } 
+  // If thereis only one wp node, and do not match, report an error
+  if (head->next == NULL) {
+    Log("watch pointing not exists");
+    return;
+  }
+
+  // 2..remaining
+  for(wp = head; wp->next != NULL; wp = wp->next) {
+    // what success goes wrong
+    if (wp->next->NO == NO) {
+      WP* backup = free_;
+      free_ = wp->next;
+      assert(wp->next != NULL);
+      wp->next = wp->next->next;
+      free_->next = backup;
+      break;
+    }
+  }
+
+  if (wp->next == NULL) {
+    Log("watch pointing not exists");
+  }
+  
+  return;
+}
+
+bool watchpoint_stop()
+{
+  bool success = true;
+  WP* wp;
+  for(wp = head; wp != NULL; wp = wp->next) {
+    // what success goes wrong
+    unsigned int n = expr(wp->exp, &success);
+    if (!success || wp->res != n) {
+      break;
+    }
+  }
+
+  // execute expression failed
+  if (!success) return true;
+  
+  // if wp != null, means expression changes
+  if (!wp) return true;
+
+  return false;
+}
+
+
+void watchpoint_display() {
+  WP* wp;
+  printf("%10s%10s", "num", "what");
+  for(wp = head; wp != NULL; wp = wp->next) {
+    // what success goes wrong
+    printf("%10d%10s\n", wp->NO, wp->exp);
+  }
+}

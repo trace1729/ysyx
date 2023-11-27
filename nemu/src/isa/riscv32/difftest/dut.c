021bb87a4bc53631d1a231eae288210c934803bb
20231011-Gong Kai Chen <1106029170@qq.com>
2023-11-27 10:03:29 +0800

    修正diff-test

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,25 +18,15 @@
 #include "../local-include/reg.h"
 #include "difftest-def.h"
 
-typedef struct diff_context_t {
-  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
-  word_t pc;
-} diff_ctx;
-
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  diff_ctx ctx;
-  int i;
-  ref_difftest_regcpy(&ctx, DIFFTEST_TO_DUT);
-  for (i = 0; i < 32; i++) {
-    if (ctx.gpr[i] != ref_r->gpr[i]) {
+  unsigned i = 0, n = MUXDEF(CONFIG_RVE, 16, 32); 
+  for (; i < n; i++) {
+    if (gpr(i) != ref_r->gpr[i]) {
       break;
     }
   }
-  if (i != 32) {
-    difftest_check_reg("", pc, ctx.gpr[i], ref_r->gpr[i]);
-  }
-  if (ctx.pc != ref_r->pc) {
-    difftest_check_reg("pc", pc, ctx.pc, ref_r->pc);
+  if (i != n) {
+    return difftest_check_reg(reg_name(i), pc, ref_r->gpr[i], gpr(i));
   }
   return true;
 }

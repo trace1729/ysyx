52576ce1ccb4f522aa3fa2893353f82bc4062925
20231011-Gong Kai Chen <1106029170@qq.com>
2023-11-30 11:36:28 +0800

    keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,11 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+  uint32_t keycode = *(uint32_t*)KBD_ADDR;
   kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  kbd->keycode = keycode;
+  if ((keycode & KEYDOWN_MASK) == KEYDOWN_MASK) {
+    kbd->keydown = 1;
+    kbd->keycode = (keycode & ~KEYDOWN_MASK);
+  } 
 }

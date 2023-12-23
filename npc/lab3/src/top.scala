416772fc3d7b08696c5d86218755ef170e9e6666
20231011-Gong Kai Chen <1106029170@qq.com>
2023-12-23 22:51:19 +0800

    重构了alu 的 adder, 将输出的数据类型换乘UInt

--- npc/lab3/src/top.scala
+++ npc/lab3/src/top.scala
@@ -5,22 +5,23 @@ class top extends Module {
 
   val io = IO(new Bundle {
     val sw       = Input(UInt(11.W))
-    val res      = Output(SInt(4.W))
+    val res      = Output(UInt(4.W))
     val overflow = Output(Bool())
     val carry    = Output(Bool())
     val zero     = Output(Bool())
   })
 
   val sel       = Wire(UInt(3.W))
-  val operand_a = Wire(SInt(4.W))
-  val operand_b = Wire(SInt(4.W))
+  val operand_a = Wire(UInt(4.W))
+  val operand_b = Wire(UInt(4.W))
+
   sel       := io.sw(10, 8) // 选择线
-  operand_a := io.sw(7, 4).asSInt
-  operand_b := io.sw(3, 0).asSInt
+  operand_a := io.sw(7, 4)
+  operand_b := io.sw(3, 0)
 
   /* 加法器 */
   val add       = Module(new adder)
-  val adder_res = Wire(SInt(4.W))
+  val adder_res = Wire(UInt(4.W))
   // 两个输入
   add.io.A   := operand_a
   add.io.B   := operand_b
@@ -33,7 +34,7 @@ class top extends Module {
 
   // 选择器
   io.res := MuxCase(
-    0.S,
+    0.U,
     Seq(
       (sel === 0.U) -> adder_res,
       (sel === 1.U) -> adder_res,
@@ -41,8 +42,8 @@ class top extends Module {
       (sel === 3.U) -> (operand_a | operand_b),
       (sel === 4.U) -> (operand_a & operand_b),
       (sel === 5.U) -> (operand_a ^ operand_b),
-      (sel === 6.U) -> (Mux((adder_res(3) ^ io.overflow).asBool, 1.S, 0.S)),
-      (sel === 7.U) -> (Mux(io.zero, 1.S, 0.S))
+      (sel === 6.U) -> (adder_res(3) ^ io.overflow),
+      (sel === 7.U) -> io.zero
     )
   )
 
@@ -50,21 +51,21 @@ class top extends Module {
 
 class adder extends Module {
   val io = IO(new Bundle {
-    val A        = Input(SInt(4.W))
-    val B        = Input(SInt(4.W))
-    val Cin      = Input(UInt(4.W))
-    val result   = Output(SInt(4.W))
+    val A        = Input(UInt(4.W))
+    val B        = Input(UInt(4.W))
+    val Cin      = Input(Bool())
+    val result   = Output(UInt(4.W))
     val overflow = Output(Bool())
     val carry    = Output(Bool())
   })
 
-  val complement_of_B   = Wire(SInt(4.W))
-  val carry_with_result = Wire(SInt(5.W))
+  val complement_of_B   = Wire(UInt(4.W))
+  val carry_with_result = Wire(UInt(5.W))
 
-  complement_of_B   := io.B ^ Cat(Seq.fill(4)(io.Cin(0))).asSInt
-  carry_with_result := Seq(io.A, complement_of_B, io.Cin.asSInt).reduce(_ +& _)
+  complement_of_B   := io.B ^ Cat(Seq.fill(4)(io.Cin))
+  carry_with_result := Seq(io.A, complement_of_B, io.Cin).reduce(_ +& _)
 
-  io.result := carry_with_result(3, 0).asSInt
+  io.result := carry_with_result(3, 0)
   io.carry  := carry_with_result(4)
   // 注意一定是 B 的补码
   io.overflow := (io.A(3) === complement_of_B(3)) && (io.result(3) =/= io.A(3))
--- npc/lab3/test/src/topSpec.scala
+++ npc/lab3/test/src/topSpec.scala
@@ -10,20 +10,20 @@ object topSpec extends ChiselUtestTester {
       testCircuit(new top) {
         dut => 
           dut.io.sw.poke("b00000010001".asUInt)
-          dut.io.res.expect(2.S)
+          dut.io.res.expect(2.U)
           dut.io.overflow.expect(0.U)
           dut.io.zero.expect(0.U)
           dut.io.carry.expect(0.U)
           dut.io.sw.poke("b00010001000".asUInt)
-          dut.io.res.expect(0.S)
+          dut.io.res.expect(0.U)
           dut.io.overflow.expect(1.U)
           dut.io.zero.expect(1.U)
           dut.io.carry.expect(1.U)
           dut.io.sw.poke("b00000000000".asUInt)
-          dut.io.res.expect(0.S)
+          dut.io.res.expect(0.U)
           dut.io.zero.expect(1.U)
           dut.io.sw.poke("b00100000000".asUInt)
-          dut.io.res.expect(0.S)
+          dut.io.res.expect(0.U)
           dut.io.zero.expect(1.U)
       }
     }
@@ -31,28 +31,28 @@ object topSpec extends ChiselUtestTester {
       testCircuit(new top) {
         dut =>
           dut.io.sw.poke("b010 0001 0000".replace(" ", "").asUInt)
-          dut.io.res.expect(-2.S)
+          dut.io.res.expect("b1110".asUInt)
           dut.io.sw.poke("b011 1001 0001".replace(" ", "").asUInt)
-          dut.io.res.expect(-7.S)
+          dut.io.res.expect("b1001".asUInt)
           dut.io.sw.poke("b100 1001 0001".replace(" ", "").asUInt)
-          dut.io.res.expect(1.S)
+          dut.io.res.expect(1.U)
           dut.io.sw.poke("b101 1001 0001".replace(" ", "").asUInt)
-          dut.io.res.expect(-8.S)
+          dut.io.res.expect("b1000".asUInt)
       }
     } 
     test("cmp") {
       testCircuit(new top) {
         dut =>
           dut.io.sw.poke("b110 0001 1000".replace(" ", "").asUInt)
-          dut.io.res.expect(0.S)
+          dut.io.res.expect(0.U)
           dut.io.sw.poke("b110 0001 0111".replace(" ", "").asUInt)
           dut.io.overflow.expect(0.U)
-          dut.io.res.expect(1.S)
+          dut.io.res.expect(1.U)
           dut.io.sw.poke("b11100000000".asUInt)
           dut.io.zero.expect(1.U)
-          dut.io.res.expect(1.S)
+          dut.io.res.expect(1.U)
           dut.io.sw.poke("b111 0000 1000".replace(" ", "").asUInt)
-          dut.io.res.expect(0.S)
+          dut.io.res.expect(0.U)
       }
     }
   }

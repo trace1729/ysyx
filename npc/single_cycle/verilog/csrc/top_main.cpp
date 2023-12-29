eb163a7db66421fe6d893319813ea96106fc899f
23060107-Gong Kai Chen <1106029170@qq.com>
2023-12-29 11:29:56 +0800

    简单实现了一键编译运行
    现在测试框架和激励代码都在一个文件里,之后应该要尝试去重构

--- npc/single_cycle/verilog/csrc/top_main.cpp
+++ npc/single_cycle/verilog/csrc/top_main.cpp
@@ -8,24 +8,8 @@
 #include "verilated_vcd_c.h" //可选，如果要导出vcd则需要加上
 
 #define BASE 0x80000000
-#define PG_ALIGN __attribute((aligned(4096)))
 
-uint32_t npc_mem[100] PG_ALIGN = {
-  0x00000413,
-  0x00009117,
-  0xffc10113,
-  0x00c000ef,
-  0x00000513,
-  0x00008067,
-  0xff410113,
-  0x00000517,
-  0x01c50513,
-  0x00112423,
-  0xfe9ff0ef,
-  0x00050513,
-  0x00100073,
-  0x0000006f,
-};
+uint8_t program[4000] = {};
 
 volatile bool end = false;
 
@@ -38,34 +22,34 @@ extern "C" void stop()
 //   return npc_mem[(pc - BASE) / 4];
 // } 
 
-// const char* img_file = getenv("IMG");
+const char* img_file;
 
-// static long load_img() {
-//   if (img_file == NULL) {
-//     printf("No image is given. Use the default build-in image.");
-//     return 4096; // built-in image size
-//   }
+static long load_img() {
+  img_file = getenv("IMG");
+  if (img_file == NULL) {
+    printf("No image is given. Use the default build-in image.\n");
+    return 4096; // built-in image size
+  }
 
-//   FILE *fp = fopen(img_file, "rb");
-//   // Assert(fp, "Can not open '%s'", img_file);
+  FILE *fp = fopen(img_file, "rb");
+  // Assert(fp, "Can not open '%s'", img_file);
 
-//   fseek(fp, 0, SEEK_END);
-//   long size = ftell(fp);
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
 
-//   printf("The image is %s, size = %ld\n", img_file, size);
+  printf("The image is %s, size = %ld\n", img_file, size);
 
-//   fseek(fp, 0, SEEK_SET);
-//   int ret = fread(npc_mem, size, 1, fp);
-//   assert(ret == 1);
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(program, size, 1, fp);
+  assert(ret == 1);
 
-//   fclose(fp);
-//   return size;
-// }
+  fclose(fp);
+  return size;
+}
 
 int main(int argc, char** argv, char** env) {
  
-  // load_img();
-  // printf("%s\n", img_file);
+  load_img();
   Verilated::commandArgs(argc, argv);
   const auto contextp = std::make_unique<VerilatedContext>();
   const auto top = std::make_unique<Vtop>(contextp.get());
@@ -88,7 +72,7 @@ int main(int argc, char** argv, char** env) {
 
     if (!top->clock) {
       printf("0x%x", top->pc);
-      top->inst = npc_mem[(top->pc - BASE) / 4];
+      top->inst = *(uint32_t *)(program + top->pc - BASE);
       printf(" 0x%x\n", top->inst);
     }
 

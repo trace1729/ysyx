08b449a3788dd2b8a64bd8449ca068da52086e98
23060107-Gong Kai Chen <1106029170@qq.com>
2023-12-29 22:52:07 +0800

    add Log system

--- npc/single_cycle/verilog/csrc/monitor/monitor.c
+++ npc/single_cycle/verilog/csrc/monitor/monitor.c
@@ -1,21 +1,45 @@
 #include <memory/paddr.h>
+#include <getopt.h>
+#include <common.h>
 
-const char* img_file;
+// void sdb_set_batch_mode();
+static char *log_file = NULL;
+// static char *diff_so_file = NULL;
+static char *img_file = NULL;
+// static char *elf_file = NULL;
+// static int difftest_port = 1234;
+
+static void welcome() {
+  Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+  IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
+        "to record the trace. This may lead to a large log file. "
+        "If it is not necessary, you can disable it in menuconfig"));
+  Log("Build time: %s, %s", __TIME__, __DATE__);
+  printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
+  printf("For help, type \"help\"\n");
+}
+
+// void init_rand();
+void init_log(const char *log_file);
+/* void init_elf(const char *elf_file); */
+/* void init_mem(); */
+/* void init_difftest(char *ref_so_file, long img_size, int port); */
+/* void init_sdb(); */
+/* void init_disasm(const char *triple); */
 
 static long load_img() {
-  img_file = getenv("IMG");
   if (img_file == NULL) {
-    printf("No image is given. Use the default build-in image.\n");
+    Log("No image is given. Use the default build-in image.");
     return 4096; // built-in image size
   }
 
   FILE *fp = fopen(img_file, "rb");
-  // Assert(fp, "Can not open '%s'", img_file);
+  Assert(fp, "Can not open '%s'", img_file);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
 
-  printf("The image is %s, size = %ld\n", img_file, size);
+  Log("The image is %s, size = %ld", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
   int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
@@ -25,7 +49,43 @@ static long load_img() {
   return size;
 }
 
-void init_monitor()
+static int parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+    {"batch"    , no_argument      , NULL, 'b'},
+    {"log"      , required_argument, NULL, 'l'},
+    {"elf"      , required_argument, NULL, 'e'},
+    {"diff"     , required_argument, NULL, 'd'},
+    {"port"     , required_argument, NULL, 'p'},
+    {"help"     , no_argument      , NULL, 'h'},
+    {0          , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+    switch (o) {
+      case 'b': printf("sdb not implemented yet\n"); break;
+      case 'p': printf("difftest not implemented yet\n"); break;
+      case 'l': log_file = optarg; break;
+      // case 'd': diff_so_file = optarg; break;
+      // case 'e': elf_file = optarg; break;
+      case 1: img_file = optarg; return 0;
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\t-b,--batch              run with batch mode\n");
+        printf("\t-l,--log=FILE           output log to FILE\n");
+        printf("\t-e,--elf=FILE           read elf FILE\n");
+        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\n");
+        exit(0);
+    }
+  }
+  return 0;
+}
+
+void init_monitor(int argc, char* argv[])
 {
-    load_img();
+  parse_args(argc, argv);
+  init_log(log_file);
+  load_img();
+  welcome();
 }

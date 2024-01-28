726fccd6a8c2e06721ebabf5667ce9b6648ecaa2
tracer-ysyx <tracer@ysyx.org>
2024-01-28 23:57:35 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.10-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Jan  5 17:38:36 UTC 2024 x86_64 GNU/Linux  23:57:35 up  4:13,  1 user,  load average: 1.67, 1.00, 0.77

--- npc/single_cycle/verilog/csrc/top_main.cpp
+++ npc/single_cycle/verilog/csrc/top_main.cpp
@@ -72,6 +72,7 @@ void verilator_exec_once(Decode* s) {
     top->clock = 0;
     top->eval();
     contextp->timeInc(1);
+    printf("current state\n");
     // tfp->dump(contextp->time());
     s->isa.inst.val = itrace.isa.inst.val;
     s->pc = itrace.pc;
@@ -84,14 +85,13 @@ void verilator_exec_once(Decode* s) {
       ftrace_block.ftrace_flag = false;
     }
 #endif
-    top->clock = 1;
-    top->eval();
-    contextp->timeInc(1);
     /* ======================================================================  */
     // ********************  next instruction state ********************
     /* ======================================================================  */
-
-    // printf("After exec instruction.\n");
+    top->clock = 1;
+    top->eval();
+    contextp->timeInc(1);
+    printf("next state\n");
 
     // tfp->dump(contextp->time());
     // s->dnpc = itrace.pc;

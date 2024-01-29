d13edc2a44dd9318948f79626927bbe85f76b3b9
tracer-ysyx <tracer@ysyx.org>
2024-01-29 15:41:10 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.10-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Jan  5 17:38:36 UTC 2024 x86_64 GNU/Linux  15:41:10 up  4:48,  1 user,  load average: 0.76, 0.75, 0.79

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -97,10 +97,12 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   
+#ifndef CONFIG_TARGET_AM
   bool stop = watchpoint_stop();
   if (stop) {
     nemu_state.state = NEMU_STOP;
   }
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {

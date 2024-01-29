b7429947bb7396491527cdfd5539a263a94bbea3
tracer-ysyx <tracer@ysyx.org>
2024-01-29 17:09:37 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.10-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Jan  5 17:38:36 UTC 2024 x86_64 GNU/Linux  17:09:37 up  6:16,  1 user,  load average: 0.71, 0.65, 0.65

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -41,11 +41,7 @@ typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
 
 typedef word_t vaddr_t;
 typedef MUXDEF(PMEM64, uint64_t, uint32_t) paddr_t;
-#ifndef CONFIG_TARGET_AM
 #define FMT_PADDR MUXDEF(PMEM64, "0x%016" PRIx64, "0x%08" PRIx32)
-#else
-#define FMT_PADDR "%x"
-#endif
 typedef uint16_t ioaddr_t;
 
 #include <debug.h>

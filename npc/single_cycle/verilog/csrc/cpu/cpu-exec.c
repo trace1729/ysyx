a94b8172f8dd8a0908b375de7ae5e5ef2b6720f0
tracer-ysyx <tracer@ysyx.org>
2024-01-29 12:11:32 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.10-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Jan  5 17:38:36 UTC 2024 x86_64 GNU/Linux  12:11:32 up  1:18,  1 user,  load average: 0.21, 0.38, 0.43

--- npc/single_cycle/verilog/csrc/cpu/cpu-exec.c
+++ npc/single_cycle/verilog/csrc/cpu/cpu-exec.c
@@ -85,6 +85,7 @@ void iringbuffer_display() {
   char (*buffer)[128] = iringbuffer.buffer;
   printf("*============ Instruction traceback ===================*\n");
   for (; front != end; ADVANCE(front)) {
+    printf("f=%d, e=%d\n", front, end);
     printf("\t%s\n", buffer[front]);
   }
   // decode_last_inst();

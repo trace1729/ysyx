ae096fe099aa574a7f2dcf019b52bf0d8c6ab510
tracer-ysyx <tracer@ysyx.org>
2024-02-29 16:42:04 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.16-2-MANJARO #1 SMP PREEMPT_DYNAMIC Sat Feb 10 09:40:02 UTC 2024 x86_64 GNU/Linux  16:42:04 up 1 day, 17:58,  1 user,  load average: 0.38, 0.40, 0.42

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,12 +5,18 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 
+#if __riscv_len == 32
+#define XLEN 4
+#else 
+#define XLEN 8
+#endif
+
 
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD; c->mepc += 4; break;
+      case -1: ev.event = EVENT_YIELD; c->mepc += XLEN; break;
       default: ev.event = EVENT_ERROR; break;
     }
 

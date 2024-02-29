c9bb014c83ab7c57bf7f660c67f441367f02b773
23060107-Gong Kai Chen <1106029170@qq.com>
2024-02-29 16:55:48 +0800

    完成 PA3.1, 但是difftest 寄了

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -13,14 +13,27 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "debug.h"
 #include <isa.h>
+#ifndef CONFIG_TARGET_AM
+#include <string.h>
+#endif
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-
-  return 0;
+  cpu.csr[MEPC] = epc;
+  cpu.csr[MCAUSE] = NO;
+#if CONFIG_ETRACE
+  char exception[128];
+  switch (NO) {
+    case -1: strcpy(exception, "YIELD"); break;
+    default: break;
+  }
+  Log("%s exception occurs\n", exception);
+#endif
+  return cpu.csr[MTVEC];
 }
 
 word_t isa_query_intr() {

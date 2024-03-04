3d7d7013758dfd6cd6813e8d1e01a4babc5f1bff
tracer-ysyx <tracer@ysyx.org>
2024-03-04 09:36:57 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.16-2-MANJARO #1 SMP PREEMPT_DYNAMIC Sat Feb 10 09:40:02 UTC 2024 x86_64 GNU/Linux  09:36:57 up 1 day, 15:01,  1 user,  load average: 1.26, 1.29, 1.23

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -68,7 +68,7 @@ void sim_t::diff_get_regs(void* diff_context) {
   ctx->csr[MSTATUS] = state->mstatus->read();
   if (state->mepc->read() != 0 ) {
     printf("spike: MEPC: %x\n", ctx->csr[MEPC]);
-    printf("nemu: mstatus: %d\n", ctx->csr[MSTATUS]);
+    printf("spike: mstatus: %d\n", ctx->csr[MSTATUS]);
   }
   if (state->mcause->read() != 0 ) {
     printf("spike: MEPC: %x\n", ctx->csr[MCAUSE]);

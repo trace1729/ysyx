5978a32c8865edf4a548d6fcb0c0e33f60c41f18
tracer-ysyx <tracer@ysyx.org>
2024-03-13 16:58:06 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  16:58:06 up 3 days,  2:06,  1 user,  load average: 0.86, 0.94, 0.74

--- npc/playground/src/Multi.scala
+++ npc/playground/src/Multi.scala
@@ -8,24 +8,18 @@ class Message extends Bundle {
 
 class IFU extends Module {
   val io = IO(new Bundle {
-    val in = Flipped(Decoupled(new Message))
+    val in = Bool()
     val out = Decoupled(new Message)
   })
 
   val pc = RegInit(0.U(32.W))
   val inst = RegInit(0.U(32.W))
 
-  // 当前模块的valid信号跟随上一个模块
-  io.out.valid := 1.U
-  
-  // 且 ready 信号跟随 valid 信号
-  io.in.ready := io.out.valid
-
-  
   // valid 就取下一条指令
-  when (io.in.valid) {
+  when (io.in) {
     pc := pc + 4.U
     inst := inst + 1.U
+    io.out.valid := 1.U
   }.otherwise {
     io.out.valid := 0.U
   }
@@ -44,33 +38,15 @@ class IFU extends Module {
 class IDU extends Module {
   val io = IO(new Bundle {
     val in = Flipped(Decoupled(new Message))
-    val out = Decoupled(new Message)
+    val out = new Message
   })
-
-  val pc = RegInit(0.U(32.W))
-  val inst = RegInit(0.U(32.W))
-
-  // 当前模块的valid信号跟随上一个模块
-  io.out.valid := io.in.valid
-
-  io.in.ready := io.out.valid
   
-  // 最后一个执行单元传递过来的信号
+  io.in.ready := io.in.valid
+
   when (io.in.valid) {
-    pc := pc + 4.U
-    inst := inst + 1.U
-  }.otherwise {
-    io.out.valid := 0.U
+    io.out := io.in.bits
   }
 
-  // 当获取到下一个单元的 ready 信号时，将指令和程序计数器传递给下一个模块
-  when (io.out.ready) {
-    io.out.bits.inst := inst
-    io.out.bits.pc := pc
-    }.otherwise {
-      io.out.bits.inst := DontCare
-      io.out.bits.pc := DontCare
-    }
 }
 
 class AsyncBus extends Module {
@@ -81,9 +57,6 @@ class AsyncBus extends Module {
   val ifu = Module(new IFU)
   val idu = Module(new IDU)
 
-  ifu.io.in <> idu.io.out
   ifu.io.out <> idu.io.in
-
-  io.out := idu.io.out.bits
-  
+  io.out := idu.io.out
 }

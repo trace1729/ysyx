e2625f168ed143c1e02ea7d43c5a73204f6ed942
tracer-ysyx <tracer@ysyx.org>
2024-03-13 21:45:34 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  21:45:34 up 41 min,  1 user,  load average: 1.55, 1.56, 1.38

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -3,5 +3,24 @@ import circt.stage._
 object Elaborate extends App {
   def top       = new AsyncBus() 
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+
+  val chiselStageOptions = Seq(
+    chisel3.stage.ChiselGeneratorAnnotation(() => top), 
+    CIRCTTargetAnnotation(CIRCTTarget.SystemVerilog)
+  )
+
+  val firtool0ptions = Seq(
+    FirtoolOption(
+      "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoSty1e=wrapInAtSquareBracket"
+    ),
+    FirtoolOption("--split-verilog"), 
+    FirtoolOption("-o=build/sv-gen"),
+    FirtoolOption("--disable-al1-randomization")
+  )
+
+  val executeOptions = chiselStageOptions ++ firtool0ptions 
+  val executeArgs = Array("-td", "build")
+  (new ChiselStage).execute(executeArgs, executeOptions)
+
+  // (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }

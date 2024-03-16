f99d73c07bef3fbebb437022352b24dafb25055e
tracer-ysyx <tracer@ysyx.org>
2024-03-16 09:17:07 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  09:17:07 up 17:00,  1 user,  load average: 1.95, 1.36, 1.12

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,7 +1,7 @@
 import circt.stage._
 
 object Elaborate extends App {
-  def top       = new AsyncBus() 
+  def top       = new CSR(10, 32) 
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
 
   val chiselStageOptions = Seq(

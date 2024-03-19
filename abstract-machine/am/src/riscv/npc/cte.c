ed792f97ff35332a29d534db81a810555abef439
23060107-Gong Kai Chen <1106029170@qq.com>
2024-03-19 11:20:35 +0800

    限定 NR_REGS 为32,不是很明白为啥定义 __riscv_e 的情况下对应16个寄存器,我这个npc也是32个寄存器呀

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -4,10 +4,19 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+#if __riscv_xlen == 32
+#define XLEN 4
+#else 
+#define XLEN 8
+#endif
+
+#define CONTEXT_SIZE  ((NR_REGS + 3 + 1) * XLEN)
+
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0xb: ev.event = EVENT_YIELD; c->mepc += XLEN; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -24,6 +33,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
+  printf("NR_REGS %d; xlen %d\n", NR_REGS, XLEN);
   // register event handler
   user_handler = handler;
 
@@ -31,15 +41,29 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  if (entry == NULL) {
+    printf("entry function is NULL!\n"); assert(0);
+  }
+  printf("NR_REGS %d\n", NR_REGS);
+  Context* ctx = (Context*)(kstack.end - CONTEXT_SIZE);
+  for (int i = 0; i < 32; i++) {
+      ctx->gpr[i] = 0;
+  }
+  ctx->mcause = 0;
+  ctx->mstatus = 0x1800;
+  // set mepc to f
+  ctx->mepc = (uintptr_t)entry;
+  // x10 -> a0 储存参数
+  ctx->gpr[10] = (uintptr_t)arg;
+  return ctx;
 }
 
 void yield() {
-#ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
-#else
+// #ifdef __riscv_e
+//  asm volatile("li a5, -1; ecall");
+// #else
   asm volatile("li a7, -1; ecall");
-#endif
+// #endif
 }
 
 bool ienabled() {

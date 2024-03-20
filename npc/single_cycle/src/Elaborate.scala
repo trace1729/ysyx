03f0478367116517bc3aac0c39ac0531773a2295
tracer-ysyx <tracer@ysyx.org>
2024-03-20 10:57:16 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  10:57:16 up 12:08,  1 user,  load average: 0.39, 0.92, 0.91

--- npc/single_cycle/src/Elaborate.scala
+++ npc/single_cycle/src/Elaborate.scala
@@ -7,6 +7,23 @@ object Elaborate extends App {
   val seperator = args.indexWhere(_ == "--")
   assert(seperator != -1, "please pass img for program to executed!")
   def top       = new top(memoryFile = args(seperator + 1))
+
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  val chiselStageOptions = Seq(
+    chisel3.stage.ChiselGeneratorAnnotation(() => top), 
+    CIRCTTargetAnnotation(CIRCTTarget.SystemVerilog)
+  )
+
+  val firtool0ptions = Seq(
+    FirtoolOption(
+      "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
+    ),
+    // FirtoolOption("--split-verilog"), 
+    // FirtoolOption("-o=build/sv-gen"),
+    FirtoolOption("--disable-all-randomization")
+  )
+
+  val executeOptions = chiselStageOptions ++ firtool0ptions 
+  val executeArgs = Array("-td", "build")
+  (new ChiselStage).execute(executeArgs, executeOptions)
 }

ba47207f60c1583fecb3fd727351acd5ffba5a72
23060107-Gong Kai Chen <1106029170@qq.com>
2024-03-20 16:30:03 +0800

    把一些帮助函数移动到 util.scala

--- npc/single_cycle/src/CPU.scala
+++ npc/single_cycle/src/CPU.scala
@@ -3,28 +3,10 @@ package cpu
 import chisel3._
 import chisel3.util._
 import cpu.config._
+import cpu.utils._
 
-class top(width: Int = 32, memoryFile: String = "") extends Module {
 
-  def padding(len: Int, v: UInt): UInt = Cat(Seq.fill(len)(v))
-  // start means the least two significant bits of read addr
-  def readDataGen(start: UInt, len: Int, data: UInt): UInt = MuxCase(
-    // lh for addr end with '11' is invaild
-    data(31, 24),
-    Seq(
-      (start === "b00".asUInt) -> data((0 + len) * 8 - 1, 0),
-      (start === "b01".asUInt) -> data((1 + len) * 8 - 1, 8),
-      (start === "b10".asUInt) -> data((2 + len) * 8 - 1, 16)
-    )
-  )
-
-  def wmaskGen(func3: UInt, addr: UInt): UInt = MuxCase(
-    "b1111".asUInt,
-    Seq(
-      (func3 === "b000".asUInt) -> (1.U << addr),
-      (func3 === "b001".asUInt) -> (3.U << addr)
-    )
-  )
+class top(memoryFile: String = "") extends Module {
 
   val io = IO(new Bundle {
     val pc   = Output(UInt(width.W))
--- npc/single_cycle/src/Datapath.scala
+++ npc/single_cycle/src/Datapath.scala
@@ -4,11 +4,46 @@ import chisel3._
 import chisel3.util._
 import cpu.config._
 
+/** ********************IFU**************************
+  */
+
+class IFUInputIO extends Bundle {
+  val pc   = Output(UInt(width.W))
+  val inst = Output(UInt(width.W))
+}
+
 class IFU extends Bundle {
-    val pc   = Output(UInt(config.width.W))
-    val inst = Output(UInt(config.width.W))
+  val out = IO(new IFUInputIO)
+}
+
+/** *******************IDU***************************
+  */
+
+class IDUOutputIO extends Bundle {
+  val rs1       = Output(UInt(width.W))
+  val rs2       = Output(UInt(width.W))
+  val immediate = Output(UInt(width.W))
 }
 
 class IDU extends Bundle {
-    val in = IO(new IFU)
+  val in = IO(Flipped(new IFUInputIO))
+  val out = IO(new IDUOutputIO)
 }
+
+
+/*********************EX***************************
+  */
+
+class EXOutputIO extends Bundle {
+    val alu_res = Output(UInt(width.W))
+}
+
+class EX extends Bundle {
+    val in = IO(Flipped(new IDUOutputIO))
+}
+
+/*********************MEM***************************
+  */
+
+/*********************WB***************************
+  */

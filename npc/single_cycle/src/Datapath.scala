ecfce122c109cc23f6f0d914c0d823be04c4cd4e
tracer-ysyx <tracer@ysyx.org>
2024-03-22 16:37:11 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  16:37:11 up 1 day,  2:01,  1 user,  load average: 1.31, 1.14, 1.04

--- npc/single_cycle/src/Datapath.scala
+++ npc/single_cycle/src/Datapath.scala
@@ -30,7 +30,10 @@ class IFU(memoryFile: String) extends Module {
   itrace.io.nextpc := wb2if_in.bits.wb_nextpc
 
   wb2if_in.ready := wb2if_in.valid
+  val tmp = RegInit(1.U)
   val ifu_valid_reg = RegInit(0.U)
+  
+  ifu_valid_reg := tmp
   if2id_out.valid := ifu_valid_reg
   
   when (wb2if_in.valid) {

c90101d31e34d2262232712b15139ae6041d793e
tracer-ysyx <tracer@ysyx.org>
2024-03-22 22:57:31 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  22:57:31 up 1 day,  8:21,  1 user,  load average: 1.00, 0.72, 0.76

--- npc/single_cycle/src/config.scala
+++ npc/single_cycle/src/config.scala
@@ -6,7 +6,7 @@ import chisel3.util._
 object config {
   val instLen = 4
   val regsNum = 32
-  val startPC: String  = "h80000000" 
+  val startPC: String  = "h80000000"
   val width: Int = 32
   val type_I :: type_I_CSRR :: type_I_CSRW :: type_ECALL :: type_MRET :: type_IL :: type_IJ :: type_IS :: type_U :: type_S :: type_J :: type_R :: type_B :: type_N :: Nil = Enum(14)
 }

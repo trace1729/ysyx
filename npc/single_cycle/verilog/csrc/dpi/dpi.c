13b3dff8d00411044c0fb5a5a59724bbc3ae5588
tracer-ysyx <tracer@ysyx.org>
2024-03-23 22:28:36 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  22:28:36 up  2:17,  1 user,  load average: 1.77, 1.15, 0.76

--- npc/single_cycle/verilog/csrc/dpi/dpi.c
+++ npc/single_cycle/verilog/csrc/dpi/dpi.c
@@ -37,7 +37,9 @@ extern "C" void Dpi_ftrace(unsigned char optype, unsigned char rd, unsigned int 
 
 extern "C" unsigned dpi_pmem_read (unsigned int raddr) {
 #if CONFIG_MTRACE
-  assert(raddr != 0);
+  if (raddr == 0) {
+    return 0;
+  }
   if (raddr >= 0x80021000) {
     printf("paddr_read: Accessing memory at location %02x\n", raddr);
   }

02f2471cfdc79a226e87989c44668d8ed961a434
tracer-ysyx <tracer@ysyx.org>
2024-03-25 18:10:22 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  18:10:22 up  8:59,  1 user,  load average: 0.62, 0.67, 0.59

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,49 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+ /* e_phentsize */
+ /*        This member holds the size in bytes of one entry  in  the  file's  program */
+ /*        header table; all entries are the same size. */
+
+// hdr -> section header table/ program header table
+
+ uintptr_t loader(PCB *pcb, const char *filename) {
+  // 读取文件
+  Elf_Ehdr e_hdr;
+  // load elf-hdr
+  size_t size = ramdisk_read(&e_hdr, 0, sizeof(e_hdr));
+  // check size
+  assert(size == sizeof(e_hdr));
+  // check elf header magic number
+  assert(*(uint32_t*)e_hdr.e_ident == 0x464C457FU);
+  // begin iterate through header table
+  for (int i = 0; i < e_hdr.e_phnum; i++) {
+    Elf_Phdr p_hdr;
+    size = ramdisk_read(&p_hdr, e_hdr.e_phoff + i * e_hdr.e_phentsize, e_hdr.e_phentsize);
+    assert(size == e_hdr.e_phentsize);
+
+    if (p_hdr.p_type != PT_LOAD) {
+      continue;
+    }
+    
+
+    
+  }
+
+
+
+
+  // 读取 elf header table
+  // iterate through elf header entry
+  //   load data to specified location
+
   return 0;
 }
 

5352bdad9029943c07cfbd425bfae83d12806b50
23060107-Gong Kai Chen <1106029170@qq.com>
2024-03-26 10:28:37 +0800

    完成 PA3.2 loader 部分

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,7 @@
 #include <proc.h>
 #include <elf.h>
+#include <stdint.h>
+#include <string.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,9 +11,49 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+#define ELF_MAGIC 0x464C457FU
+
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+ /* e_phentsize */
+ /*        This member holds the size in bytes of one entry  in  the  file's  program */
+ /*        header table; all entries are the same size. */
+
+// hdr -> section header table/ program header table
+
+ uintptr_t loader(PCB *pcb, const char *filename) {
+  // 读取文件
+  Elf_Ehdr e_hdr;
+  // load elf-hdr
+  size_t size = ramdisk_read(&e_hdr, 0, sizeof(e_hdr));
+  // check size
+  assert(size == sizeof(e_hdr));
+  // check elf header magic number
+  assert(*(uint32_t*)e_hdr.e_ident == ELF_MAGIC);
+  // begin iterate through header table
+  printf("program header table:\n");
+
+  for (int i = 0; i < e_hdr.e_phnum; i++) {
+    Elf_Phdr p_hdr;
+    size = ramdisk_read(&p_hdr, e_hdr.e_phoff + i * e_hdr.e_phentsize, e_hdr.e_phentsize);
+    assert(size == e_hdr.e_phentsize);
+
+    // load segment only when segment type is PT_LOAD
+    if (p_hdr.p_type != PT_LOAD) {
+      continue;
+    }
+    printf("p_vaddr: %d, p_paddr: %d, Filesize: %x, Memsize:%x\n",  p_hdr.p_vaddr, p_hdr.p_paddr, p_hdr.p_filesz, p_hdr.p_memsz);
+  
+    // 将程序从 elf 文件拷贝到 对应的物理地址 (这里物理地址和虚拟地址是一样的)
+    ramdisk_read((void *)(uintptr_t)(p_hdr.p_paddr), p_hdr.p_offset, p_hdr.p_filesz);
+    // 将多余的空间设置为0
+    memset((void *)(uintptr_t)(&p_hdr.p_paddr + p_hdr.p_filesz), 0, p_hdr.p_memsz - p_hdr.p_filesz);
+
+  }
+
+  return e_hdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {

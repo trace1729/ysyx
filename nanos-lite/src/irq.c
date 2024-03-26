4241f8f9baafbeb3facd410802e7345bc4a3712b
tracer-ysyx <tracer@ysyx.org>
2024-03-26 11:43:25 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  11:43:25 up 1 day,  2:32,  1 user,  load average: 1.02, 0.88, 0.86

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,8 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("yield\n"); break;
-    case EVENT_SYSCALL: do_syscall(c);break;
+    case EVENT_SYSCALL: do_syscall(c); break;
+    case EVENT_ERROR: break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 

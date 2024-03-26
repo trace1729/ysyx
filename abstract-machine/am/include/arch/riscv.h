f54a9eecc644b7865eaa6b688ac2cc64b0b63b88
tracer-ysyx <tracer@ysyx.org>
2024-03-26 11:52:41 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  11:52:41 up 1 day,  2:41,  1 user,  load average: 0.53, 0.72, 0.79

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -18,9 +18,9 @@ struct Context {
 #define GPR1 gpr[17] // a7
 #endif
 
-#define GPR2 gpr[11]
-#define GPR3 gpr[12]
-#define GPR4 gpr[13]
-#define GPRx gpr[0]
+#define GPR2 gpr[10]
+#define GPR3 gpr[11]
+#define GPR4 gpr[12]
+#define GPRx gpr[10]
 
 #endif
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,14 +7,28 @@ int sys_yield()
   return 0;
 }
 
+int sys_exit(int status) 
+{
+  halt(status);
+  return 0;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
+  int result_code = 0;
 
   switch (a[0]) {
-    case SYS_yield: sys_yield();
+    case SYS_yield: result_code = sys_yield(); break;
+    case SYS_exit: result_code = sys_exit(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  c->GPRx = result_code;
 }
 
 

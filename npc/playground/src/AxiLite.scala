f374ffcb40fe0a22d55a759c8313b7b5770bb977
tracer-ysyx <tracer@ysyx.org>
2024-03-28 22:12:32 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  22:12:32 up 13:19,  1 user,  load average: 1.34, 0.79, 0.78

--- npc/playground/src/AxiLite.scala
+++ npc/playground/src/AxiLite.scala
@@ -33,8 +33,8 @@ object AxiLiteReadData {
 }
 
 class AxiLiteSlave(val addrWidth: Int, val dataWidth: Int) extends Bundle {
-  val readAddr = Flipped(Decoupled(AxiLiteAddr(addrWidth)))
-  val readData = Decoupled(AxiLiteReadData(dataWidth))
+  // val readAddr = Flipped(Decoupled(AxiLiteAddr(addrWidth)))
+  // val readData = Decoupled(AxiLiteReadData(dataWidth))
 
   val writeAddr = Flipped(Decoupled(AxiLiteAddr(addrWidth)))
   val writeData = Flipped(Decoupled(AxiLiteWriteData(dataWidth)))

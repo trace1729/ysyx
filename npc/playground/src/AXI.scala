36c4a6aa728f259f0df0479c88c9341d2afd39e8
tracer-ysyx <tracer@ysyx.org>
2024-03-28 22:28:59 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  22:28:59 up 13:35,  1 user,  load average: 1.95, 1.51, 1.20

--- npc/playground/src/AXI.scala
+++ npc/playground/src/AXI.scala
@@ -74,7 +74,7 @@ class SRAM extends Module {
   // the data will be available on the next rising edge after valid and ready is both asserted,
   // How to tell the SRAM this feature?
   // using wmask to distinguish
-  val ram = VecInit(Seq.fill(10)(Wire(0.U(10.W))))
+  val ram = VecInit(Seq.fill(10)(Wire(UInt(10.W))))
 
   in.writeResp.valid := 0.U
   when(in.writeData.bits.strb =/= 0.U) {

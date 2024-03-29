00b2b92dd7f641ee982a7ae888fe311c0bf1742a
tracer-ysyx <tracer@ysyx.org>
2024-03-29 09:44:31 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  09:44:31 up 1 day, 51 min,  2 users,  load average: 1.89, 1.10, 0.98

--- npc/playground/src/AXI.scala
+++ npc/playground/src/AXI.scala
@@ -80,7 +80,7 @@ class SRAM extends Module {
   out := 0.U
   when(in.writeData.bits.strb =/= 0.U) {
     in.writeResp.valid          := 1.U
-    in.writeResp := 0.U
+    in.writeResp.bits := 0.U
     out := in.writeData.bits.data
   }
 

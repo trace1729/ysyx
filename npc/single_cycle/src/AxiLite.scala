a2279f70ba643c22668f8b63a6c2b202cf2b0ee5
tracer-ysyx <tracer@ysyx.org>
2024-03-31 10:38:34 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  10:38:34 up 15:11,  1 user,  load average: 2.00, 1.72, 1.45

--- npc/single_cycle/src/AxiLite.scala
+++ npc/single_cycle/src/AxiLite.scala
@@ -1,4 +1,5 @@
 // referenced from https://github.com/nhynes/chisel3-axi.git
+package cpu
 
 import chisel3._
 import chisel3.util._
@@ -57,4 +58,85 @@ class AxiLiteMaster(val addrWidth: Int, val dataWidth: Int) extends Bundle {
 object AxiLiteMaster {
   def apply(addrWidth: Int, dataWidth: Int) =
     new AxiLiteMaster(addrWidth = addrWidth, dataWidth = dataWidth)
+}
+
+object AxiState extends ChiselEnum {
+  val aIDLE, aWRITE, aREAD, aACK = Value
+}
+
+class ExternalInput extends Bundle {
+  val externalMemRW   = Flipped(Bool())
+  val externalMemEn   = Flipped(Bool())
+  val externalValid   = Flipped(Bool())
+  val externalData    = Flipped(UInt(32.W))
+  val externalWmask   = Flipped(UInt(4.W))
+  val externalAddress = Flipped(UInt(32.W))
+}
+
+object ExternalInput {
+  def apply() = new ExternalInput
+}
+
+object AxiController {
+  def apply(addrWidth: Int, dataWidth: Int) = new AxiController(addrWidth, dataWidth)
+}
+
+class AxiController(addrWidth: Int, dataWidth: Int) extends Module {
+  // 控制器的输入应该是可以通用化的
+  val in  = IO(ExternalInput())
+  val axi = IO(AxiLiteMaster(addrWidth, dataWidth))
+
+  // external data is stored in these two registers
+  // when axiMaster.axi.valid and ready is both asserted,
+  // these registers will update their value using external
+  // address and data in the following rising edge
+  // and since register's output is directly connected to
+  // the sram, sram will receive the data once the register
+  // updates its value.
+  import AxiState._
+
+  // initial is idle state
+  val state   = RegInit(aIDLE)
+  val dataWen = (state === aWRITE)
+  val addrWen = (state === aWRITE)
+
+  // axi.writeData.bits.data := Re
+  // in one way or the other, you will going to learn how to build a finite state machine
+
+  axi.writeAddr.valid := 0.U
+  axi.writeData.valid := 0.U
+
+  switch(state) {
+    is(aIDLE) {
+      when(in.externalValid && in.externalMemEn) {
+        state := Mux(in.externalMemRW, aWRITE, aREAD)
+      }
+    }
+    is (aREAD) {
+      axi.readAddr.valid := 1.U
+      when (axi.readAddr.valid && axi.readAddr.ready) {
+        state := aACK
+      }
+    }
+    is(aWRITE) {
+      axi.writeAddr.valid := 1.U
+      axi.writeData.valid := 1.U
+      when (axi.writeData.valid && axi.writeData.ready) {
+        state := aACK
+      }
+    }
+    is (aACK) {
+      when (axi.writeResp.ready && axi.writeResp.valid) {
+        state := aIDLE
+      }
+    }
+
+  }
+  axi.writeResp.ready     := axi.writeResp.valid
+  axi.writeData.bits.data := in.externalData
+  axi.writeData.bits.strb := in.externalWmask
+  axi.writeAddr.bits.addr := in.externalAddress
+
+  // 逐渐领会到状态机的写法
+
 }

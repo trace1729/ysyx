5876edabd37a739c476dc0bb3ba43dcfc8b7253d
tracer-ysyx <tracer@ysyx.org>
2024-04-03 15:57:29 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  15:57:29 up 18:58,  2 users,  load average: 0.48, 0.49, 0.57

--- npc/single_cycle/verilog/csrc/top_main.cpp
+++ npc/single_cycle/verilog/csrc/top_main.cpp
@@ -85,8 +85,8 @@ int main(int argc, char** argv, char** env) {
   sim_reset(top.get());
 
   init_monitor(argc, argv);
-  sdb_mainloop();
-  // dummy();
+  // sdb_mainloop();
+  dummy();
   sim_end();
   Log("gracefully quit");
   

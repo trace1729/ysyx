9c53c6d24e0017af86ec92e78e3ce499c7b77c18
tracer-ysyx <tracer@ysyx.org>
2024-04-03 20:00:02 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  20:00:02 up 23:01,  2 users,  load average: 1.27, 1.00, 0.81

--- npc/single_cycle/src/Datapath.scala
+++ npc/single_cycle/src/Datapath.scala
@@ -16,14 +16,14 @@ class DatapathIO extends Bundle {
 class Datapath(memoryFile: String) extends Module {
   val io = IO(new DatapathIO)
 
-  val ifu = Module(new IFU(memoryFile))
-  val idu = Module(new IDU)
-  val ex  = Module(new EX)
-  val mem = Module(new LSU)
-  val wb  = Module(new WB)
+  val ifu     = Module(new IFU(memoryFile))
+  val idu     = Module(new IDU)
+  val ex      = Module(new EX)
+  val mem     = Module(new LSU)
+  val wb      = Module(new WB)
+  val arbiter = Module(new Arbiter)
 
   val sram = Module(new SRAM)
-  
 
   ifu.if2idOut <> idu.if2idIn
   idu.id2exOut <> ex.id2exIn
@@ -32,10 +32,9 @@ class Datapath(memoryFile: String) extends Module {
   wb.wb2ifuOut <> ifu.wb2ifIn
 
   // for axi interface
-  
-  ifu.ifuAxiOut <> sram.ifuIn
-  mem.lsuAxiOut <> sram.lsuIn
-  sram.ifuEnable := ifu.ifuEnable
+  arbiter.ifuIn <> ifu.ifuAxiOut
+  arbiter.lsuIn <> mem.lsuAxiOut
+  arbiter.out <> sram.in
 
   // 诡异的连线，上面各阶段之间的握手突出一个毫无意义 (确定 pc 和 寄存器的写回值)
   idu.data           := wb.wb2ifuOut.bits.wbData
@@ -49,3 +48,26 @@ class Datapath(memoryFile: String) extends Module {
   io.pc   := ifu.if2idOut.bits.pc
 }
 
+class Arbiter extends Module {
+  val ifuIn = IO(Flipped(AxiLiteMaster(width, width)))
+  val lsuIn = IO(Flipped(AxiLiteMaster(width, width)))
+  val out   = IO(AxiLiteMaster(width, width))
+
+  out := DontCare
+
+  // 默认将 ready 置为 false
+  ifuIn.readAddr.ready  := false.B
+  ifuIn.writeAddr.ready := false.B
+  ifuIn.writeData.ready := false.B
+
+  lsuIn.readAddr.ready  := false.B
+  lsuIn.writeAddr.ready := false.B
+  lsuIn.writeData.ready := false.B
+
+  when(ifuIn.writeAddr.valid || ifuIn.writeData.valid || ifuIn.readAddr.valid) {
+    out <> ifuIn
+  }.elsewhen(lsuIn.writeAddr.valid || lsuIn.writeData.valid || lsuIn.readAddr.valid) {
+    out <> lsuIn
+  }
+
+}
--- npc/single_cycle/src/SRAM.scala
+++ npc/single_cycle/src/SRAM.scala
@@ -17,24 +17,15 @@ object SRAMState extends ChiselEnum {
 }
 
 class SRAM extends Module {
-  val ifuEnable = IO(Input(Bool()))
-  val ifuIn     = IO(Flipped(AxiLiteMaster(width, width)))
-  val lsuIn     = IO(Flipped(AxiLiteMaster(width, width)))
-  val dmem      = Module(new Dmem(width))
+
+  val in   = IO(Flipped(AxiLiteMaster(width, width)))
+  val dmem = Module(new Dmem(width))
 
   // ready follows valid
 
-  when(ifuEnable) {
-    ifuIn.writeAddr.ready := ifuIn.writeAddr.valid
-    ifuIn.writeData.ready := ifuIn.writeData.valid
-    ifuIn.readAddr.ready  := ifuIn.readAddr.valid
-    lsuIn                 := DontCare
-  }.otherwise {
-    lsuIn.writeAddr.ready := lsuIn.writeAddr.valid
-    lsuIn.writeData.ready := lsuIn.writeData.valid
-    lsuIn.readAddr.ready  := lsuIn.readAddr.valid
-    ifuIn                 := DontCare
-  }
+  in.writeAddr.ready := in.writeAddr.valid
+  in.writeData.ready := in.writeData.valid
+  in.readAddr.ready  := in.readAddr.valid
 
   import SRAMState._
 
@@ -47,78 +38,61 @@ class SRAM extends Module {
   val mtime = RegInit(UInt(64.W), 0.U)
   mtime := mtime + 1.U
 
-
   // 直接设置一个计数器，来模拟延迟，每一条指令的执行周期都不一样，这样也算模拟了随机延迟了。
   // TODO 为 valid / ready 信号设置一个随机延迟
   // val timer = RegInit(0.U(32.W))
   // timer := Mux(timer === 10.U, 0.U, timer + 1.U)
 
-  dmem.io.raddr := Mux(ifuEnable, ifuIn.readAddr.bits.addr, lsuIn.readAddr.bits.addr)
-  dmem.io.waddr := Mux(ifuEnable, ifuIn.writeAddr.bits.addr, lsuIn.writeAddr.bits.addr)
-  dmem.io.wdata := Mux(ifuEnable, ifuIn.writeData.bits.data, lsuIn.writeData.bits.data)
-  dmem.io.wmask := Mux(ifuEnable, ifuIn.writeData.bits.strb, lsuIn.writeData.bits.strb)
+  dmem.io.raddr := in.readAddr.bits.addr
+  dmem.io.waddr := in.writeAddr.bits.addr
+  dmem.io.wdata := in.writeData.bits.data
+  dmem.io.wmask := in.writeData.bits.strb
 
   dmem.io.memRW     := 0.U
   dmem.io.memEnable := false.B
 
-  ifuIn.readData.bits.data := dmem.io.rdata
-  lsuIn.readData.bits.data := MuxCase(
+  in.readData.bits.data := MuxCase(
     dmem.io.rdata,
     Seq(
-      (lsuIn.readAddr.bits.addr === config.RTC_MNIO.U) -> mtime(31, 0),
-      (lsuIn.readAddr.bits.addr === (config.RTC_MNIO + 4).U) -> mtime(63, 32)
+      (in.readAddr.bits.addr === config.RTC_MNIO.U) -> mtime(31, 0),
+      (in.readAddr.bits.addr === (config.RTC_MNIO + 4).U) -> mtime(63, 32)
     )
   )
 
-  when(ifuEnable) {
-    ifuIn.writeResp.valid    := false.B
-    ifuIn.readData.valid     := false.B
-    ifuIn.readData.bits.resp := 1.U
-    ifuIn.writeResp.bits     := 1.U
-    lsuIn                    := DontCare
-  }.otherwise {
-    lsuIn.writeResp.valid    := false.B
-    lsuIn.readData.valid     := false.B
-    lsuIn.readData.bits.resp := 1.U
-    lsuIn.writeResp.bits     := 1.U
-    ifuIn                    := DontCare
-  }
+  in.writeResp.valid    := false.B
+  in.readData.valid     := false.B
+  in.readData.bits.resp := 1.U
+  in.writeResp.bits     := 1.U
 
   // using a state machine would elegantly represent
   // the whole axi interface communicating process
   switch(state) {
     is(aIDLE) {
       // received write data and address concurrently
-      when(ifuEnable) {
-        when(ifuIn.readAddr.ready && ifuIn.readAddr.valid) {
-          state := aREAD
-        }
-      }.otherwise {
-        when(lsuIn.writeAddr.ready && lsuIn.writeAddr.valid && lsuIn.writeData.valid && lsuIn.writeData.ready) {
-          state := Mux(lsuIn.writeAddr.bits.addr === config.SERAL_MNIO.U, aUART, awriteDataAddr)
-        }.elsewhen(lsuIn.writeData.ready && lsuIn.writeData.valid) {
-          state := awriteData
-        }.elsewhen(lsuIn.writeAddr.ready && lsuIn.writeAddr.valid) {
-          state := awriteAddr
-        }
-        when(lsuIn.readAddr.ready && lsuIn.readAddr.valid) {
-          state := Mux(
-            (lsuIn.readAddr.bits.addr === config.RTC_MNIO.U) || (lsuIn.readAddr.bits.addr === (config.RTC_MNIO + 4).U),
-            aRTC,
-            aREAD
-          )
-        }
+      when(in.writeAddr.ready && in.writeAddr.valid && in.writeData.valid && in.writeData.ready) {
+        state := Mux(in.writeAddr.bits.addr === config.SERAL_MNIO.U, aUART, awriteDataAddr)
+      }.elsewhen(in.writeData.ready && in.writeData.valid) {
+        state := awriteData
+      }.elsewhen(in.writeAddr.ready && in.writeAddr.valid) {
+        state := awriteAddr
+      }
+      when(in.readAddr.ready && in.readAddr.valid) {
+        state := Mux(
+          (in.readAddr.bits.addr === config.RTC_MNIO.U) || (in.readAddr.bits.addr === (config.RTC_MNIO + 4).U),
+          aRTC,
+          aREAD
+        )
       }
     }
     // only received write addr
     is(awriteData) {
-      when(lsuIn.writeAddr.ready && lsuIn.writeAddr.valid) {
+      when(in.writeAddr.ready && in.writeAddr.valid) {
         state := awriteDataAddr
       }
     }
     // only received write data
     is(awriteAddr) {
-      when(lsuIn.writeData.ready && lsuIn.writeData.valid) {
+      when(in.writeData.ready && in.writeData.valid) {
         state := awriteDataAddr
       }
     }
@@ -132,7 +106,7 @@ class SRAM extends Module {
       state := aReadACK
     }
     is(aUART) {
-      printf("%c", lsuIn.writeData.bits.data(7, 0))
+      printf("%c", in.writeData.bits.data(7, 0))
       state := aUARTACK
     }
     is(aRTC) {
@@ -140,50 +114,42 @@ class SRAM extends Module {
       state         := aRTCACK
     }
     is(aUARTACK) {
-      lsuIn.writeResp.valid := true.B
-      lsuIn.writeResp.bits  := 0.U
-      when(lsuIn.writeResp.ready && lsuIn.writeResp.valid) {
+      in.writeResp.valid := true.B
+      in.writeResp.bits  := 0.U
+      when(in.writeResp.ready && in.writeResp.valid) {
         state := aIDLE
       }
     }
     is(aRTCACK) {
-      lsuIn.readData.valid     := 1.U
-      lsuIn.readData.bits.resp := 0.U
-      when(lsuIn.readData.ready && lsuIn.readData.valid) {
+      in.readData.valid     := 1.U
+      in.readData.bits.resp := 0.U
+      when(in.readData.ready && in.readData.valid) {
         state := aIDLE
       }
     }
     // finished write/read transaction
     is(aWriteACK) {
       // when (timer === 0.U) {
-      dmem.io.memEnable     := true.B
-      dmem.io.memRW         := 1.U
-      lsuIn.writeResp.valid := true.B
-      lsuIn.writeResp.bits  := 0.U
-      when(lsuIn.writeResp.ready && lsuIn.writeResp.valid) {
+      dmem.io.memEnable  := true.B
+      dmem.io.memRW      := 1.U
+      in.writeResp.valid := true.B
+      in.writeResp.bits  := 0.U
+      when(in.writeResp.ready && in.writeResp.valid) {
         state := aIDLE
       }
       // }
     }
     is(aReadACK) {
       // when (timer === 0.U) {
-      dmem.io.memRW     := 0.U
-      dmem.io.memEnable := true.B
-      when(ifuEnable) {
-        ifuIn.readData.valid     := 1.U
-        ifuIn.readData.bits.resp := 0.U
-        when(ifuIn.readData.ready && ifuIn.readData.valid) {
-          state := aIDLE
-        }
-      }.otherwise {
-        lsuIn.readData.valid     := 1.U
-        lsuIn.readData.bits.resp := 0.U
-        when(lsuIn.readData.ready && lsuIn.readData.valid) {
-          state := aIDLE
-        }
+      dmem.io.memRW         := 0.U
+      dmem.io.memEnable     := true.B
+      in.readData.valid     := 1.U
+      in.readData.bits.resp := 0.U
+      when(in.readData.ready && in.readData.valid) {
+        state := aIDLE
       }
-      // }
     }
+    // }
   }
 }
 

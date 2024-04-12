2f3c1d54afcc4e53329795649609afd5159e4ca1
tracer-ysyx <tracer@ysyx.org>
2024-04-12 17:22:21 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  17:22:21 up 1 day, 21:14,  1 user,  load average: 1.20, 0.99, 0.89

--- npc/single_cycle/verilog/csrc/top_main.cpp
+++ npc/single_cycle/verilog/csrc/top_main.cpp
@@ -70,7 +70,7 @@ static void dummy() {
     top->clock = 1;
     top->eval();
     contextp->timeInc(1);
-    // tfp->dump(contextp->time());
+    tfp->dump(contextp->time());
 
     if (nemu_state.state == NEMU_END) {
       Log("execution ended at cycle %d", num_i);

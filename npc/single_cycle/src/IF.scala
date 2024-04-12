b4458a9161cd40abbad1e632cfdc6f8bec1cc805
tracer-ysyx <tracer@ysyx.org>
2024-04-12 17:14:37 +0800

    > generate verilog ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.19-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri Mar  1 18:16:16 UTC 2024 x86_64 GNU/Linux  17:14:37 up 1 day, 21:06,  1 user,  load average: 0.74, 0.80, 0.81

--- npc/single_cycle/src/IF.scala
+++ npc/single_cycle/src/IF.scala
@@ -44,16 +44,7 @@ class IFU(memoryFile: String) extends Module {
     PC := nextPC
   }
 
-  // 为 axiController 设置默认值
-  axiController.stageInput.writeAddr      := DontCare
-  axiController.stageInput.writeData      := DontCare
-  axiController.stageInput.writeResp      := DontCare
-  axiController.stageInput.readAddr.valid := (ifu_state === stageState.sWaitReady)
-
-  // 处理器 read ack 请求
-  axiController.stageInput.readData.ready := axiController.stageInput.readData.valid
-  val readCompleted = axiController.stageInput.readData.valid && axiController.stageInput.readData.ready
-
+  // 处理 ifu 的状态转移
   import stageState._
   val ifu_state = RegInit(sIDLE)
 
@@ -68,6 +59,17 @@ class IFU(memoryFile: String) extends Module {
       }
     }
   }
+
+  // 为 axiController 设置默认值
+  axiController.stageInput.writeAddr      := DontCare
+  axiController.stageInput.writeData      := DontCare
+  axiController.stageInput.writeResp      := DontCare
+  axiController.stageInput.readAddr.valid := (ifu_state === stageState.sWaitReady)
+
+  // 处理器 read ack 请求
+  axiController.stageInput.readData.ready := axiController.stageInput.readData.valid
+  val readCompleted = axiController.stageInput.readData.valid && axiController.stageInput.readData.ready
+
 
   axiController.stageInput.readAddr.bits.addr := nextPC
   if2idOut.bits.inst                          := axiController.stageInput.readData.bits.data

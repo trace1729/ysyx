d2f75183b92c9c0baee62e4f27864c956d9b31ae
tracer-ysyx <tracer@ysyx.org>
2024-07-05 17:44:19 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.32-1-MANJARO #1 SMP PREEMPT_DYNAMIC Mon May 27 03:41:25 UTC 2024 x86_64 GNU/Linux  17:44:19 up 1 day,  6:51,  1 user,  load average: 0.57, 0.57, 0.51

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -22,8 +22,12 @@ int sys_yield()
 
 int sys_exit(int status) 
 {
+#ifdef strace
+    printf("%s(%d)\n", syscalls[SYS_exit], status);
+#endif
   halt(status);
-  return 0;
+  while(1);
+  return -1;
 }
 
 int sys_write(uintptr_t fd, uintptr_t buf, uintptr_t count) {
@@ -52,7 +56,7 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     case SYS_yield: result_code = sys_yield(); break;
-    case SYS_exit: result_code = sys_exit(a[1]); break;
+    case SYS_exit:  sys_exit(a[1]); break;
     case SYS_write: result_code = sys_write(a[1], a[2], a[3]); break;
     case SYS_brk: result_code = sys_brk(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);

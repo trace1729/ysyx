0688366295f211b0f4e02daf138e714ffae332be
tracer-ysyx <tracer@ysyx.org>
2024-07-05 16:48:40 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.32-1-MANJARO #1 SMP PREEMPT_DYNAMIC Mon May 27 03:41:25 UTC 2024 x86_64 GNU/Linux  16:48:40 up 1 day,  5:55,  1 user,  load average: 0.49, 0.64, 0.65

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,12 +5,9 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
+  while (j != 100) {
+		j ++;
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
     }
-  }
   return 0;
 }

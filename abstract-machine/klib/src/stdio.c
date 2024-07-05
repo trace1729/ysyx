c5fe4767d8e177eb47e8c2e488b5125cb38bbe9a
tracer-ysyx <tracer@ysyx.org>
2024-07-05 17:02:59 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.32-1-MANJARO #1 SMP PREEMPT_DYNAMIC Mon May 27 03:41:25 UTC 2024 x86_64 GNU/Linux  17:02:59 up 1 day,  6:10,  1 user,  load average: 0.48, 0.45, 0.52

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -240,7 +240,7 @@ int vsprintf(char* buffer, const char* fmt, va_list ap) {
           prefix += (type >> 4);
           pl = 2;
         }
-        p = MAX(p, end - start + !arg.i);
+        p = MAX(p, 2*sizeof(void*));
         break;
       case 'd': case 'i':
         pl = 1; // 负号

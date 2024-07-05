f238cbdc81eca16c3569dd8e7846e2ef2039a19a
tracer-ysyx <tracer@ysyx.org>
2024-07-05 20:17:17 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.32-1-MANJARO #1 SMP PREEMPT_DYNAMIC Mon May 27 03:41:25 UTC 2024 x86_64 GNU/Linux  20:17:17 up  1:02,  1 user,  load average: 0.18, 0.28, 0.25

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -77,8 +77,6 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
 
-  return (void*)-1;
- 
   static char* program_break = &end;
   assert (program_break != 0);
   char* old_break = program_break;

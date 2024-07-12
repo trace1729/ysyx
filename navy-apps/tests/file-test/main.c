c6bce7a7ecd3380acd13e99e28dee72366c0573c
tracer-ysyx <tracer@ysyx.org>
2024-07-12 22:50:17 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.34-1-MANJARO #1 SMP PREEMPT_DYNAMIC Wed Jun 19 19:00:06 UTC 2024 x86_64 GNU/Linux  22:50:17 up  7:46,  2 users,  load average: 0.41, 0.31, 0.30

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,42 +2,39 @@
 #include <assert.h>
 
 int main() {
-  printf("before\n");
   FILE *fp = fopen("/share/files/num", "r+");
-  printf("after\n");
   assert(fp);
 
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-  assert(size == 5000);
-
-  fseek(fp, 500 * 5, SEEK_SET);
-  int i, n;
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1);
-  }
-
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
-    fprintf(fp, "%4d\n", i + 1 + 1000);
-  }
-
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1);
-  }
-
-  printf("seek_set %d\n", SEEK_SET);
-  fseek(fp, 0, 0);
-  for (i = 0; i < 500; i ++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1 + 1000);
-  }
-
-  fclose(fp);
-
-  printf("PASS!!!\n");
+  /* fseek(fp, 0, SEEK_END); */
+  /* long size = ftell(fp); */
+  /* assert(size == 5000); */
+  /*  */
+  /* fseek(fp, 500 * 5, SEEK_SET); */
+  /* int i, n; */
+  /* for (i = 500; i < 1000; i ++) { */
+  /*   fscanf(fp, "%d", &n); */
+  /*   assert(n == i + 1); */
+  /* } */
+  /*  */
+  /* fseek(fp, 0, SEEK_SET); */
+  /* for (i = 0; i < 500; i ++) { */
+  /*   fprintf(fp, "%4d\n", i + 1 + 1000); */
+  /* } */
+  /*  */
+  /* for (i = 500; i < 1000; i ++) { */
+  /*   fscanf(fp, "%d", &n); */
+  /*   assert(n == i + 1); */
+  /* } */
+  /*  */
+  /* fseek(fp, 0, SEEK_SET); */
+  /* for (i = 0; i < 500; i ++) { */
+  /*   fscanf(fp, "%d", &n); */
+  /*   assert(n == i + 1 + 1000); */
+  /* } */
+  /*  */
+  /* fclose(fp); */
+  /*  */
+  /* printf("PASS!!!\n"); */
 
   return 0;
 }

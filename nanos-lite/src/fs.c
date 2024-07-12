326e94358f5a872500c7f309bcee622ad089b9fc
tracer-ysyx <tracer@ysyx.org>
2024-07-12 19:59:42 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.34-1-MANJARO #1 SMP PREEMPT_DYNAMIC Wed Jun 19 19:00:06 UTC 2024 x86_64 GNU/Linux  19:59:42 up  4:55,  2 users,  load average: 0.62, 0.42, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -71,7 +71,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
-  assert(offset > 0);
+ //  assert(offset > 0);
 
   switch (whence) {
   case SEEK_CUR:

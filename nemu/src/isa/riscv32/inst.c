8afa345ec30b3b62c36b1d6c308a602f9de412bb
tracer-ysyx <tracer@ysyx.org>
2024-07-16 11:17:52 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.34-1-MANJARO #1 SMP PREEMPT_DYNAMIC Wed Jun 19 19:00:06 UTC 2024 x86_64 GNU/Linux  11:17:52 up  1:11,  2 users,  load average: 0.36, 0.22, 0.20

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -62,6 +62,7 @@ void ftrace(int rd, int type, Decode* s, word_t src1) {
       || memcmp(function, "putch", strlen(function)) == 0 
       || memcmp(function, "sprintf", strlen(function)) == 0 
       || memcmp(function, "memcpy", strlen(function)) == 0 
+      || memcmp(function, "vprintf", strlen(function)) == 0 
     ) {
     return;
   }

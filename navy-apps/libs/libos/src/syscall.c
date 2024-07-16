71a2cce0d7168b7aadd9908de9c2e09502fa9da9
tracer-ysyx <tracer@ysyx.org>
2024-07-16 11:31:57 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.34-1-MANJARO #1 SMP PREEMPT_DYNAMIC Wed Jun 19 19:00:06 UTC 2024 x86_64 GNU/Linux  11:31:57 up  1:25,  2 users,  load average: 0.34, 0.43, 0.36

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -100,7 +100,6 @@ int _close(int fd) {
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  printf("navy: whence: %d\n", whence);
   int cnt = _syscall_(SYS_lseek, fd, offset, whence);
   return cnt;
 }

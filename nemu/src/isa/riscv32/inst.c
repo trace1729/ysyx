8984df0dbef8234524a8ff44ceac8328fd9ae567
tracer-ysyx <tracer@ysyx.org>
2024-07-16 11:16:02 +0800

    > compile NEMU ysyx_23060107 龚开宸 Linux trace-pavilion 6.6.34-1-MANJARO #1 SMP PREEMPT_DYNAMIC Wed Jun 19 19:00:06 UTC 2024 x86_64 GNU/Linux  11:16:02 up  1:09,  2 users,  load average: 0.09, 0.15, 0.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -57,15 +57,19 @@ int depth = 0;
 void get_function_symbol_by_address(uint32_t addr, char *buf);
 void ftrace(int rd, int type, Decode* s, word_t src1) {
   char function[128];
-
+  get_function_symbol_by_address(s->pc, function);
+  if (memcmp(function, "printf", strlen(function)) == 0
+      || memcmp(function, "putch", strlen(function)) == 0 
+    ) {
+    return;
+  }
+  printf("0x%x: ", s->pc);
+  for (int i = 0; i < depth; i++) printf(" ");
   //      jalr          x0              x1
   if (type == JALR && rd == ZERO && src1 == R(RA)) {
     // if register is x0, and instructio type is jalr
     // then it is function return
     // return from instead of return to
-    get_function_symbol_by_address(s->pc, function);
-    printf("0x%x: ", s->pc);
-    for (int i = 0; i < depth; i++) printf(" ");
     printf("ret[%s]\n", function);
     depth--;
 
@@ -74,14 +78,6 @@ void ftrace(int rd, int type, Decode* s, word_t src1) {
           || (type == JALR && rd == RA)) {
     // if register is ra, and instruction type is jal(r)
     // then it is function call
-    get_function_symbol_by_address(s->dnpc, function);
-    if (memcmp(function, "printf", strlen(function)) == 0
-        || memcmp(function, "putch", strlen(function)) == 0 
-      ) {
-      return;
-    }
-    printf("0x%x: ", s->pc);
-    for (int i = 0; i < depth; i++) printf(" ");
     printf("call[%s@0x%x]\n", function, s->dnpc);
     depth++;
   }

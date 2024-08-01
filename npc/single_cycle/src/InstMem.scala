77bf38e9afb4cc5d87911a30849cd242c9ef098c
23060107-Gong Kai Chen <1106029170@qq.com>
2024-08-01 21:07:58 +0800

    refacor(npc): remove chisel mem

--- npc/single_cycle/src/InstMem.scala
+++ npc/single_cycle/src/InstMem.scala
@@ -3,7 +3,6 @@ import chisel3._
 import chisel3.util._
 import cpu.config._
 import chisel3.util.experimental.loadMemoryFromFileInline
-import firrtl.annotations.MemoryLoadFileType
 
 /* 
     width: 指令宽度
@@ -19,17 +18,12 @@ class InstMemIO (width: Int, ilen: Int) extends Bundle {
     val inst = Output(Vec(ilen, UInt(InstMem.byte_len.W)))
 }
 
-class InstMem(val memoryFile: String = "") extends Module {
+class InstMem extends Module {
     val memSize = 8192 * 4 * 8
     val ilen = width / InstMem.byte_len
 
     val io = IO(new InstMemIO(width, ilen))
     val mem = Mem(memSize, UInt(InstMem.byte_len.W))
-
-    // Initialize memory
-    if (memoryFile.trim().nonEmpty) {
-        loadMemoryFromFileInline(mem, memoryFile, MemoryLoadFileType.Binary)
-    }
 
     io.inst := VecInit(
         mem.read(io.pc - config.startPC.U + 3.U),
